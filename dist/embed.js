55e80414ebf7a9eb086b8a7f62ea97648625c72d
Anthony Ho <ahomnky11@gmail.com>
2023-09-27 18:38:15 +0100

    dist

--- dist/embed.js
+++
@@ -1,6 +0,0 @@
-var s =
-  "!!=mjol!isfg>#iuuqt;00deo/wpoubof/dpn0tuzmf/dtt#!sfm>#tuzmftiffu#!0?!!!!=tdsjqu!tsd>#iuuqt;00deo/ubjmxjoedtt/dpn#?=0tdsjqu?!!!!=tdsjqu!tsd>#iuuqt;00deo/wpoubof/dpn0xjehfu/njo/kt#?=0tdsjqu?";
-var m = "";
-for (var i = 0; i < s.length; i++)
-  m += String.fromCharCode(s.charCodeAt(i) - 1);
-document.write(m);
