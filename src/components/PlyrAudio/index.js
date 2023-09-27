7c2bd81897211862427d9359cf0f2227b1af9126
Anthony Ho <ahomnky11@gmail.com>
2023-09-27 17:46:48 +0100

    widget min.js

--- src/components/PlyrAudio/index.js
+++ src/components/PlyrAudio/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Plyr from "plyr-react";
-import "plyr-react/plyr.css";
+
 import styled, { useTheme } from "styled-components";
 
 const StyledPlyr = styled.div`
