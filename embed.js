7c97c8828efa02aebc132f1282bd7a316979013f
Anthony Ho <ahomnky11@gmail.com>
2023-10-03 13:12:18 +0100

    https

--- embed.js
+++ embed.js
@@ -1,5 +1,5 @@
 var s =
-  "!=mjol!isfg>#iuuq;00deo/wpoubof/dpn0tuzmf/dtt#!sfm>#tuzmftiffu#!0?!=tdsjqu!tsd>#iuuq;00deo/wpoubof/dpn0xjehfu/njo/kt#?=0tdsjqu?";
+  "!=mjol!isfg>#iuuqt;00deo/wpoubof/dpn0tuzmf/dtt#!sfm>#tuzmftiffu#!0?!=tdsjqu!tsd>#iuuqt;00deo/wpoubof/dpn0xjehfu/njo/kt#?=0tdsjqu?";
 var m = "";
 for (var i = 0; i < s.length; i++)
   m += String.fromCharCode(s.charCodeAt(i) - 1);
