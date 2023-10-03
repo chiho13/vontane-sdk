68e037a1ace35b5f0776ec2322ed5850b9cf2f84
Anthony Ho <ahomnky11@gmail.com>
2023-10-03 13:04:04 +0100

    defer

--- embed.js
+++ embed.js
@@ -1,5 +1,5 @@
 var s =
-  "!=mjol!isfg>#tuzmf/dtt#!sfm>#tuzmftiffu#!0?!!!!=tdsjqu!tsd>#xjehfu/njo/kt#?=0tdsjqu?";
+  "!=mjol!isfg>#tuzmf/dtt#!sfm>#tuzmftiffu#!0?!!!!=tdsjqu!tsd>#xjehfu/njo/kt#!efgfs?=0tdsjqu?";
 var m = "";
 for (var i = 0; i < s.length; i++)
   m += String.fromCharCode(s.charCodeAt(i) - 1);
