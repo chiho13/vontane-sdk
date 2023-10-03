3e8090d0972123b92177576876ac9fe3dd0eead4
Anthony Ho <ahomnky11@gmail.com>
2023-10-03 13:02:14 +0100

    embed

--- embed.js
+++ embed.js
@@ -1,5 +1,5 @@
 var s =
-  "!!=mjol!isfg>#iuuqt;00deo/wpoubof/dpn0tuzmf/dtt#!sfm>#tuzmftiffu#!0?!!!!=tdsjqu!tsd>#iuuqt;00deo/ubjmxjoedtt/dpn#?=0tdsjqu?!!!!=tdsjqu!tsd>#iuuqt;00deo/wpoubof/dpn0xjehfu/njo/kt#?=0tdsjqu?";
+  "!=mjol!isfg>#tuzmf/dtt#!sfm>#tuzmftiffu#!0?!!!!=tdsjqu!tsd>#xjehfu/njo/kt#?=0tdsjqu?";
 var m = "";
 for (var i = 0; i < s.length; i++)
   m += String.fromCharCode(s.charCodeAt(i) - 1);
